7d6394b1891fa83a9a9140074e6bb0a99d96ca60
Joona Heinilä <joona.heinila@gmail.com>
2023-08-19 14:39:59 +0300

    osa6: 6.1 & 6.2

---
+++ osa6/unicafe-redux/src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createStore } from "redux";
+
+import reducer from "./reducer";
+
+const store = createStore(reducer);
+
+const App = () => {
+
+  const registerFeedback = (type) => {
+    store.dispatch({ type });
+  };
+
+  return (
+    <div>
+      <button onClick={() => registerFeedback("GOOD")}>good</button>
+      <button onClick={() => registerFeedback("OK")}>ok</button>
+      <button onClick={() => registerFeedback("BAD")}>bad</button>
+      <button onClick={() => registerFeedback("ZERO")}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok} </div>
+      <div>bad {store.getState().bad} </div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const renderApp = () => {
+  root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);
