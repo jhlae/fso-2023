2f62d60be992c53d128138aa1bf495a312045561
Joona Heinilä <joona.heinila@gmail.com>
2023-09-12 00:01:58 +0300

    osa 5: 5.20–5.23

--- osa5/cypress/e2e/blog_app.cy.js
+++ osa5/cypress/e2e/blog_app.cy.js
@@ -49,4 +49,73 @@ describe("When logged in", function () {
     cy.visit("http://localhost:3000/");
     cy.contains("A test blog post created by a testing tool");
   });
+
+  it("A blog can be liked", function () {
+    cy.createBlog({
+      title: "A second blog post created by a testing tool",
+      author: "Cypress",
+      url: "https://google.com/second",
+    });
+    cy.contains("A second blog post created by a testing tool")
+      .parent()
+      .find("button")
+      .click();
+    cy.get("#like-btn").click();
+  });
+
+  it("A blog can be deleted", function () {
+    cy.contains("A second blog post created by a testing tool")
+      .parent()
+      .find("button")
+      .click();
+    cy.get("#delete-btn").click();
+    cy.get("html").should("contain", "Blog was removed");
+  });
+
+  it("The blogs are ordered by the amount of likes, from highest count to lowest", function () {
+    cy.createBlog({
+      title: "1: Lorem Ipsum",
+      author: "Cypress",
+      url: "https://www.google.com/search?q=lorem+ipsum",
+    });
+    cy.createBlog({
+      title: "2: Dolor Sit Amet",
+      author: "Cypress",
+      url: "https://www.google.com/search?q=dolor+sit+amet",
+    });
+    cy.createBlog({
+      title: "3: Consectetur Adipiscing Elit",
+      author: "Cypress",
+      url: "https://www.google.com/search?q=consectetur+adipiscing+elit",
+    });
+
+    // https://docs.cypress.io/api/commands/wait
+    // wait 1s between the like clicks
+
+    cy.contains("Consectetur Adipiscing Elit").parent().find("button").click(); // Open toggle menu
+    cy.get("#like-btn")
+      .click()
+      .wait(1000)
+      .click()
+      .wait(1000)
+      .click()
+      .wait(1000)
+      .click()
+      .wait(1000); // like count = 4
+    cy.contains("Consectetur Adipiscing Elit").parent().find("button").click(); // Close toggle menu
+
+    cy.contains("Lorem Ipsum").parent().find("button").click(); // Open toggle menu
+    cy.get("#like-btn").click().wait(1000).click(); // like count = 2
+    cy.contains("Lorem Ipsum").parent().find("button").click(); // Close toggle menu
+
+    cy.contains("Dolor Sit Amet").parent().find("button").click(); // Open toggle menu
+    cy.get("#like-btn").click().wait(1000); // like count = 1
+    cy.contains("Dolor Sit Amet").parent().find("button").click(); // Close toggle menu
+
+    cy.get(".blog-details")
+      .eq(0)
+      .should("contain", "Consectetur Adipiscing Elit");
+    cy.get(".blog-details").eq(1).should("contain", "Lorem Ipsum");
+    cy.get(".blog-details").eq(2).should("contain", "Dolor Sit Amet");
+  });
 });
